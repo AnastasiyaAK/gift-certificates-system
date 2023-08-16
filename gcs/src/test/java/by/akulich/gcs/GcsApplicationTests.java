e5e166237e45a02ce2d553b457582587524d4b9d
AnastasiaAK <asyapetya11@gmail.com>
2023-08-16 11:00:58 +0300

    add module gcs

---
+++ gcs/src/test/java/by/akulich/gcs/GcsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.akulich.gcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
